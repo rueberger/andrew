b5864b389a9a690a87d88b408b2928c5282cb55d
Andrew Berger <andberger@berkeley.edu>
2016-06-29 14:01:51 -0400

    preparing for andrew v 0.01

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext as _build_ext
+import os
+
+setup(name='andrew',
+      version='0.01',
+      description='the portable Andrew',
+      url='https://github.com/rueberger/andrew',
+      author='Andrew Berger',
+      author_email='andbberger@gmail.com',
+      license='MIT',
+      install_requires=],
+      setup_requires=[],
+      packages=['andrew'],
+      zip_safe=False)
